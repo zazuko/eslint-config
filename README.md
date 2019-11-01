38c344312641f3b681ab6dde8ba783166537606c
tpluscode <tpluscode@users.noreply.github.com>
2019-11-01 11:44:54 +0100

    feat: first version based on current projects

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# eslint-config
+# @zazuko/eslint-config
+
+Slightly customized `standard` config with TypeScript rules enabled. 
+
+## Installation
+
+```
+npm i --save-dev \
+    @zazuko/eslint-config \
+    @typescript-eslint/eslint-plugin \
+    @typescript-eslint/parser \
+    standard
+```
+
+## Usage
+
+In your eslint config:
+
+```json
+{
+  "extends": [ "@zazuko" ],
+  "parser": {
+    "project": "./tsconfig.json"
+  }
+}
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint/eslint-plugin'
+  ],
+  rules: {
+    indent: 'off',
+    'import/no-unresolved': 'warn',
+    'import/extensions': 'off',
+    'space-before-function-paren': [
+      'error',
+      {
+        named: 'never'
+      }
+    ],
+    'comma-dangle': [
+      'error',
+      'always-multiline'
+    ],
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none'
+        }
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: [
+        '*.conf.js',
+        '*.config.js'
+      ],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
+}
